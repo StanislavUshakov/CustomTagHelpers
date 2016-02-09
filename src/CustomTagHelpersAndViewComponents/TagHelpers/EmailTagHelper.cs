7768988de49396ba2aa42029153fea73db00ca42
Stanislav Ushakov <sushakov@dataart.com>
2016-02-09 13:52:43 +0300

    Initial commit - EmailTagHelper

---
+++ src/CustomTagHelpersAndViewComponents/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.Razor.TagHelpers;
+using System;
+
+namespace CustomTagHelpersAndViewComponents.TagHelpers
+{
+    /// <summary>
+    /// Custom TagHelper for generating email links
+    /// </summary>
+    public class EmailTagHelper : TagHelper
+    {
+        private const string EmailDomain = "example.com";
+
+        /// <summary>
+        /// The email address without domain
+        /// </summary>
+        public string MailTo { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            output.TagName = "a";
+            string address = MailTo + "@" + EmailDomain;
+            output.Attributes["href"] = "mailto:" + address;
+            output.Content.SetContent(address);
+        }
+    }
+}
