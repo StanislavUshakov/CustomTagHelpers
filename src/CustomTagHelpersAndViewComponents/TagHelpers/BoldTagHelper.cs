46cf95ec5c3ceca6a610c330e46beef7395921f5
Stanislav Ushakov <sushakov@dataart.com>
2016-02-10 13:46:40 +0300

    Added BoldTagHelper.

---
+++ src/CustomTagHelpersAndViewComponents/TagHelpers/BoldTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Razor.TagHelpers;
+using System;
+
+namespace CustomTagHelpersAndViewComponents.TagHelpers
+{
+    /// <summary>
+    /// Custom tag helper for making text inside different HTML tags bold
+    /// </summary>
+    [HtmlTargetElement("bold")]
+    [HtmlTargetElement(Attributes = "bold")]
+    public class BoldTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            output.Attributes.RemoveAll("bold");
+            output.PreContent.SetHtmlContent("<strong>");
+            output.PostContent.SetHtmlContent("</strong>");
+        }
+    }
+}
