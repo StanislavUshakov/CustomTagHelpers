d7d8a06c55ca00e863668f2dc6ae9fa147d0043d
Stanislav Ushakov <sushakov@dataart.com>
2016-02-10 17:42:17 +0300

    Added CopyrightTagHelper.

---
+++ src/CustomTagHelpersAndViewComponents/TagHelpers/CopyrightTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+using Microsoft.AspNet.Razor.TagHelpers;
+
+namespace CustomTagHelpersAndViewComponents.TagHelpers
+{
+    [HtmlTargetElement("copyright")]
+    public class CopyrightTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var content = await output.GetChildContentAsync();
+            string copyright = $"<p>&copy; {DateTime.Now.Year} {content.GetContent()}";
+            output.Content.SetHtmlContent(copyright);
+        }
+    }
+}
